2e67f51694fb00828db0d09042ee99905bf2da1e
Esity <matthewdiverson@gmail.com>
2026-05-06 16:44:02 -0500

    Silence spec logging during test gates

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,5 @@ require 'bundler/setup'
 require 'legion/extensions/llm'
 
 require 'legion/extensions/llm/vertex'
+
+Legion::Logging.setup(level: 'fatal', log_file: File::NULL, log_stdout: false, async: false, color: false)
